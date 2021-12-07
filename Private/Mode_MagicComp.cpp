a40281d4323f80447c22e5ddbc684a77d7ec691c
광현 최 <helirang34@gmail.com>
2021-12-08 03:14:32 +0900

    MagicComp_Exception_Fix

--- Private/Mode_MagicComp.cpp
+++ Private/Mode_MagicComp.cpp
@@ -11,14 +11,11 @@ UMode_MagicComp::UMode_MagicComp()
 	SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
 }
 
-
-// Called when the game starts
 void UMode_MagicComp::BeginPlay()
 {
 	Super::BeginPlay();
 	
 	GetOwner();
-	// ...
 
 	ACharacter* TempOwner = Cast<ACharacter>(GetOwner());
 
@@ -34,33 +31,38 @@ void UMode_MagicComp::BeginPlay()
 	}
 }
 
+// MagicKinds 요청 마법 계열 @Todo MagicGrade 등과 같이 등급을 나눠서 추가 예정.
+// ex -> grade 1 == 파이어볼, grade 2 == 파이어스톰 등
 void UMode_MagicComp::MagicQuitFire(EMagicEnum MagicKinds)
 {
-	UClass* FireMagic;
+	UClass* FireMagic = nullptr;
 	AActor* MyOwner = GetOwner();
 
 	switch (MagicKinds)
 	{
 	case EMagicEnum::E_Fire:
-		if(FireMagicProjectiles[MagicMap_Comp[MagicKinds]] != nullptr)
+		if (FireMagicProjectiles.Num() != 0) //@Todo 요청하는 스킬 등급 세분화, 해당 등급에 따라 Num 측정
 		FireMagic = FireMagicProjectiles[MagicMap_Comp[MagicKinds]];
 		break;
 	case EMagicEnum::E_Water:
-		if (WaterMagicProjectiles[MagicMap_Comp[MagicKinds]] != nullptr)
+		if (WaterMagicProjectiles.Num() != 0)
 		FireMagic = WaterMagicProjectiles[MagicMap_Comp[MagicKinds]];
 		break;
 	case EMagicEnum::E_Earth:
-		if (EarthMagicProjectiles[MagicMap_Comp[MagicKinds]] != nullptr)
+		if (EarthMagicProjectiles.Num() != 0)
 		FireMagic = EarthMagicProjectiles[MagicMap_Comp[MagicKinds]];
 		break;
 	default:
-		if (FireMagicProjectiles[MagicMap_Comp[MagicKinds]] != nullptr)
+		if (FireMagicProjectiles.Num() != 0)
 		FireMagic = FireMagicProjectiles[MagicMap_Comp[MagicKinds]];
 		break;
 	}
-
-	FVector tempVector = MyOwner->GetActorLocation() + MyOwner->GetActorForwardVector() * 180;
-	FRotator tempRotatior = MyOwner->GetActorRotation();
-	GetWorld()->SpawnActor(FireMagic, &tempVector, &tempRotatior, SpawnInfo);
+	
+	if (FireMagic != nullptr)
+	{
+		FVector tempVector = MyOwner->GetActorLocation() + MyOwner->GetActorForwardVector() * 180;
+		FRotator tempRotatior = MyOwner->GetActorRotation();
+		GetWorld()->SpawnActor(FireMagic, &tempVector, &tempRotatior, SpawnInfo);
+	}
 }
 
